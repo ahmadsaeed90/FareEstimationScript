cf2d87f0f2ddbeb11cee44afde3c9288f67dd908
Muhammad Ahmad Saeed <ahmadsaeed90@gmail.com>
2021-01-11 19:16:31 +0400

    Update FareCalculatorService.java

--- src/main/java/com/beat/fareestimation/service/FareCalculatorService.java
+++ src/main/java/com/beat/fareestimation/service/FareCalculatorService.java
@@ -1,16 +1,13 @@
 package com.beat.fareestimation.service;
 
 import com.beat.fareestimation.constant.Constants;
-import com.beat.fareestimation.constant.RideState;
 import com.beat.fareestimation.model.Position;
 import com.beat.fareestimation.model.Ride;
 import com.beat.fareestimation.util.MathUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class FareCalculatorService implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(FareCalculatorService.class);
@@ -80,23 +77,17 @@ public class FareCalculatorService implements Runnable {
             prev = current;
         }
 
-        /*
-        for (int i = 1; i < positions.size(); i++) {
-            fare += calculateFare(positions.get(i-1), positions.get(i));
-        }
-         */
         return Math.max(fare, Constants.MinRideFare);
+    }
+
+    public double processRide(Ride ride) {
+        removeDuplicates(ride.getPositions());
+        return calculateFare(ride.getPositions());
     }
 
     @Override
     public void run() {
-        /*for (int i = 0; i < 1000000; i++) {
-            System.out.println("Thread" + Thread.currentThread().getId() + "::" + i);
-        }*/
-
-        removeDuplicates(this.ride.getPositions());
-        var fare = calculateFare(this.ride.getPositions());
-
-        //logger.info("Fare for ride " + ride.getRideId() + " = " + fare);
+        var fare = processRide(this.ride);
+        logger.info("Fare for ride " + ride.getRideId() + " = " + fare);
     }
 }
