3b0bad1f7c08bc22286bdca21d5555986a5b10f4
Muhammad Ahmad Saeed <ahmadsaeed90@gmail.com>
2021-01-14 22:14:14 +0400

    Update IInputProcessor.java

--- src/main/java/com/beat/fareestimation/service/processor/IInputProcessor.java
+++ src/main/java/com/beat/fareestimation/service/processor/IInputProcessor.java
@@ -4,5 +4,5 @@ import java.io.InputStreamReader;
 import java.io.Writer;
 
 public interface IInputProcessor {
-    public void process(InputStreamReader fileName, Writer writer) throws Exception;
+    void process(InputStreamReader fileName, Writer writer) throws Exception;
 }
